ac1e60b4058eca7b99a0c50c7ec137bfede348c8
Suraj Kushwah <suraj@bugswriter.com>
2022-10-03 16:59:22 +0530

    making storage dir

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import os
 import uvicorn 
 from app import app
+from app.config import *
 
 if __name__=="__main__":
+	os.makedirs(f"{STORAGE_DIR}", exist_ok=True)
 	uvicorn.run(app, host="0.0.0.0", port=8002)
